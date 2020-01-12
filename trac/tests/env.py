629b32e60f3cfab22c77cb1068d393ff1212845a
Jun Omae <jun66j5@gmail.com>
2020-01-13 03:43:57 +0900

    (1.5-py3) use `configparser` instead of `ConfigParser`

--- trac/tests/env.py
+++ trac/tests/env.py
@@ -11,7 +11,7 @@
 # individuals. For the exact contribution history, see the revision
 # history and logs, available at https://trac.edgewall.org/log/.
 
-from ConfigParser import RawConfigParser
+from configparser import RawConfigParser
 from glob import glob
 from subprocess import PIPE, Popen
 import inspect
