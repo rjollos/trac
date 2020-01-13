c8570b8865972767dfca9c2a57460eba43defbf4
Jun Omae <jun66j5@gmail.com>
2020-01-13 13:14:22 +0900

    (1.5-py3) use `list()` for `dict.{keys,values,items}()`

--- trac/web/tests/main.py
+++ trac/web/tests/main.py
@@ -101,7 +101,8 @@ class AuthenticateTestCase(unittest.TestCase):
     @classmethod
     def tearDownClass(cls):
         from trac.core import ComponentMeta
-        for component in cls.authenticators.values() + cls.request_handlers:
+        for component in list(cls.authenticators.values()) + \
+                         cls.request_handlers:
             ComponentMeta.deregister(component)
 
     def setUp(self):
@@ -408,7 +409,7 @@ class PostProcessRequestTestCase(unittest.TestCase):
     @classmethod
     def tearDownClass(cls):
         from trac.core import ComponentMeta
-        for component in cls.request_filter.values():
+        for component in list(cls.request_filter.values()):
             ComponentMeta.deregister(component)
 
     def setUp(self):
