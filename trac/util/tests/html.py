b5ac3abbbba4da6317d4f660c43979b48709afb9
Jun Omae <jun66j5@gmail.com>
2020-01-28 15:54:06 +0900

    (1.5-py3) remove unnecessary `from __future__` statements on Python 3

--- trac/util/tests/html.py
+++ trac/util/tests/html.py
@@ -11,8 +11,6 @@
 # individuals. For the exact contribution history, see the revision
 # history and logs, available at https://trac.edgewall.org/log/.
 
-from __future__ import unicode_literals
-
 import doctest
 import io
 import sys
