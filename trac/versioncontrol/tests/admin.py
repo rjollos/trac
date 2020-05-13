cc2e5c88e68c6f595fd7760ace06f8dbab7882de
Ryan J Ollos <ryan.j.ollos@gmail.com>
2020-05-12 19:26:24 -0700

    Add test coverage

--- trac/versioncontrol/tests/admin.py
+++ trac/versioncontrol/tests/admin.py
@@ -13,10 +13,12 @@
 
 import unittest
 
+from trac.admin.web_ui import AdminModule
 from trac.core import Component, implements
 from trac.test import EnvironmentStub, MockRequest
 from trac.versioncontrol.api import DbRepositoryProvider, IRepositoryConnector
 from trac.versioncontrol.admin import RepositoryAdminPanel
+from trac.web.api import RequestDone
 
 
 class RepositoryAdminPanelTestCase(unittest.TestCase):
@@ -95,6 +97,87 @@ class VersionControlAdminTestCase(unittest.TestCase):
         self.assertEqual('', repositories['']['alias'])
         self.assertEqual('blah', repositories['blah']['name'])
         self.assertEqual('', repositories['blah']['alias'])
+
+    def _add_repository(self, name, type_, path):
+        req = MockRequest(
+                self.env, method='POST',
+                path_info='/admin/versioncontrol/repository',
+                args={
+                    'add_repos': 'Add',
+                    'dir': path,
+                    'name': name,
+                    'type': type_,
+                })
+
+        mod = AdminModule(self.env)
+        self.assertTrue(mod.match_request(req))
+        with self.assertRaises(RequestDone):
+            mod.process_request(req)
+
+        return req
+
+    def _get_repositories(self):
+        req = MockRequest(
+                self.env, method='GET',
+                path_info='/admin/versioncontrol/repository')
+
+        mod = AdminModule(self.env)
+        self.assertTrue(mod.match_request(req))
+        data = mod.process_request(req)[1]
+
+        return data
+
+    def assert_repository_added(self, req):
+        self.assertEqual([], req.chrome['warnings'])
+        self.assertEqual(['303 See Other'], req.status_sent)
+        self.assertEqual(
+                'http://example.org/trac.cgi/admin/versioncontrol/repository',
+                req.headers_sent['Location'])
+
+    def assert_repository_exists(self, name, type_, path, data):
+        repositories = data['repositories']
+        self.assertEqual(name, repositories[name]['name'])
+        self.assertEqual(type_, repositories[name]['type'])
+        self.assertEqual(path, repositories[name]['dir'])
+
+    def test_add_repository(self):
+        name = 'proj1'
+        type_ = ''
+        path = '/path/to/repos'
+        req = self._add_repository(name, type_, path)
+        self.assert_repository_added(req)
+        data = self._get_repositories()
+        self.assert_repository_exists(name, type_, path, data)
+
+    def test_add_repository_with_spaces(self):
+        name = 'proj1'
+        type_ = ''
+        path = '/path/to/repos'
+        req = self._add_repository(' %s ' % name, type_, ' %s ' % path)
+        self.assert_repository_added(req)
+        data = self._get_repositories()
+        self.assert_repository_exists(name, type_, path, data)
+
+    def test_add_repository_with_zwsp(self):
+        name = 'proj1'
+        type_ = ''
+        path = '/path/to/repos'
+        req = self._add_repository(u'\u200b%s\u200b' % name, type_,
+                                   u'\u200b%s\u200b' % path)
+        self.assert_repository_added(req)
+        data = self._get_repositories()
+        self.assert_repository_exists(name, type_, path, data)
+
+    def test_add_repository_non_absolute_path(self):
+        name = 'proj1'
+        type_ = ''
+        path = 'path/to/repos'
+        req = self._add_repository(name, type_, path)
+        self.assertEqual(
+                ['The repository directory must be an absolute path.'],
+                req.chrome['warnings'])
+        data = self._get_repositories()
+        self.assertEqual({}, data['repositories'])
 
 
 def test_suite():
